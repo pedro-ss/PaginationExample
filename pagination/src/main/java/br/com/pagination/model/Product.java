322102071532b36e309f926540fa74c1118019fc
marrare <rivaldo.alvesb36@gmail.com>
2022-01-24 22:14:07 -0300

    fix :bug: remove old class and created methods gets and sets

--- pagination/src/main/java/br/com/pagination/model/Product.java
+++ pagination/src/main/java/br/com/pagination/model/Product.java
@@ -32,5 +32,31 @@ public class Product {
     @Column(name = "NAME")
     private String name;
     @Column(name = "PRICE")
-    private Double price; 
+    private Double price;
+    
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+    
 }
